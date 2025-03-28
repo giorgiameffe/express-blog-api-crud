71c3834adbbac1f29c63cc3d81274edfc4ed7b01
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-28 18:20:56 +0100

    feat: complete second milestone

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -4,12 +4,13 @@ const posts = require('../data/posts.js');
 // funzioni operazioni CRUD
 
 function index (req, res) {
-    res.send('Lista dei post')
+    res.json(posts);
 }
 
 function show (req, res) {
-    const {id} = req.params;
-    res.send(`Dettagli del post con id ${id}`);
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+    res.json(post);
 }
 
 function store (req,res) {
@@ -17,19 +18,31 @@ function store (req,res) {
 }
 
 function update (req,res) {
-    const {id} = req.params;
+    const id = req.params.id;
     res.send(`Modifica integrale del post con id ${id}`);
 }
 
 function modify (req, res) {
-    const {id} = req.params;
+    const id = req.params.id;
     res.send(`Modifica parziale del post con id ${id}`);
     
 }
 
 function destroy (req, res) {
-    const {id} = req.params;
-    res.send(`Eliminazione del post con id ${id}`);
+
+    const id = parseInt(req.params.id);
+    const post = posts.findIndex(post => post.id === id);
+    posts.splice(post, 1);
+    console.log(posts);
+
+    // status 
+    if(post) {
+        res.status(204);
+
+        return res.json({
+            status: 204
+        })
+    }
 }
 
 // esportazione 
