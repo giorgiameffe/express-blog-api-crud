bb72150203907c448de4d57fef9579971a839b02
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-28 16:39:22 +0100

    feat: create routers folder, import express and add routes

---
+++ routers/posts-router.js
@@ -0,0 +1,41 @@
+// importare express
+const express = require('express');
+
+// funzione router 
+const router = express.Router();
+
+// Creare rotte per le operazioni CRUD
+
+// index 
+router.get('/', (req, res) => {
+    res.send('Lista dei post')
+})
+
+//show 
+router.get('/:id', (req,res) => {
+    const {id} = req.params;
+    res.send(`Dettagli del post con id ${id}`);
+})
+
+// store 
+router.post('/', (res,req) => {
+    res.send('Creazione di un nuovo post');
+})
+
+// update 
+router.put('/:id', (req,res) => {
+    const {id} = req.params;
+    res.send(`Modifica integrale del post con id ${id}`);
+})
+
+// modify 
+router.patch('/:id', (req,res) => {
+    const {id} = req.params;
+    res.send(`Modifica parziale del post con id ${id}`);
+})
+
+// destroy 
+router.destroy('/:id', (req, res) => {
+    const {id} = req.params;
+    res.send(`Eliminazione del post con id ${id}`);
+})
